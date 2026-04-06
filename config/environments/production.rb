f1446605b081ffe5d141014eb39ac565693402ac
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-06 15:22:53 -0300

    Portal Relatórios Render 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,10 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+
+  config.require_master_key = false
+  config.public_file_server.enabled = true
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
