1f30e15762f960e90892d766ee186aea6573972a
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-06 15:41:58 -0300

    Disable solid cable/queue/cache

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module PortalRelatorios
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.legacy_connection_handling = true
   end
 end
