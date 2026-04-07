46262c3b5e1ab37430d7481ecbf0423d03e75ce3
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-07 09:52:25 -0300

    Temporary fix for root and layout 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "health#index"
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "devise/sessions#new", as: :unauthenticated_root
+  end
+
   get "/dashboard", to: "dashboard#index"
 end
