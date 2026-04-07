f011d66d553b99dc335f2d8cdb324b1e890fa519
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-07 14:00:27 -0300

    Fix unauthenticated root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   unauthenticated do
-    root to: "devise/sessions#new", as: :unauthenticated_root
+    root to: redirect("/users/sign_in"), as: :unauthenticated_root
   end
 
   get "/dashboard", to: "dashboard#index"
