04b867e28c0c3c5740c6bb81a750de94c1ac86a4
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-07 09:33:48 -0300

    Temporary fix for root and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  authenticated :user do
-    root "dashboard#index", as: :authenticated_root
+  root to: redirect("/users/sign_in")
 
-    get "/dashboard", to: "dashboard#index"
-
-    resources :sidebar_sections, only: [:show]
-    resources :report_pages, only: [:index, :show]
-  end
-
-  unauthenticated do
-    root to: redirect("/users/sign_in"), as: :unauthenticated_root
-  end
-
-  namespace :admin do
-    resources :sidebar_sections
-    resources :report_pages
-  end
+  get "/dashboard", to: "dashboard#index"
 end
