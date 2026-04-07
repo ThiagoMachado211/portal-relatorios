018982991eda7ec19eab6dcb0ac37eedc092c6e3
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-07 09:45:21 -0300

    Temporary fix for root and layout 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    authenticated_root_path
-  end
-
-  def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path
+    dashboard_path
   end
 end
