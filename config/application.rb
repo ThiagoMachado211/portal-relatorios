e9aa3a73fa58a6756e18b8f97f698e0503d26c29
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-08 19:57:02 -0300

    Problemas no propshaft 2

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,23 @@
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "propshaft/railtie"
-# require "action_cable/engine"
+require_relative "boot"
+
+require "rails/all"
 
 Bundler.require(*Rails.groups)
 
 module PortalRelatorios
   class Application < Rails::Application
     config.load_defaults 8.0
+
+    # Timezone
+    config.time_zone = "America/Sao_Paulo"
+
+    # Evita erro com propshaft
+    config.assets.enabled = true
+
+    # Active Storage
+    config.active_storage.variant_processor = :vips
+
+    # Autoload
+    config.autoload_lib(ignore: %w[assets tasks])
   end
 end
