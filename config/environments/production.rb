a171af975243f0160a82dc8351bbcaed0e1be42e
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-09 11:21:53 -0300

    Corrigindo o Report Page 8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
 
   config.consider_all_requests_local = false
   config.public_file_server.enabled = true
+  config.active_storage.resolve_model_to_route = :rails_storage_proxy
 
   config.assets.compile = false
 
