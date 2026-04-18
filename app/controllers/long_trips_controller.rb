71042fec4744abb0138b3ecbdef715d1e9c80002
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-18 11:24:23 -0300

    Estruturando a aba de trechos_longos 3

--- app/controllers/long_trips_controller.rb
+++ app/controllers/long_trips_controller.rb
@@ -48,4 +48,45 @@ class LongTripsController < ApplicationController
       :refund_value_points
     )
   end
+
+  def dashboard
+    @long_trips = LongTrip.all
+
+    air_trips = @long_trips.select { |trip| trip.transport_mode.to_s.downcase == "aéreo" || trip.transport_mode.to_s.downcase == "aereo" }
+    land_trips = @long_trips.select { |trip| trip.transport_mode.to_s.downcase == "rodoviário" || trip.transport_mode.to_s.downcase == "rodoviario" || trip.transport_mode.to_s.downcase == "terrestre" }
+
+    @total_air_tickets = air_trips.count
+    @total_land_tickets = land_trips.count
+
+    air_days = air_trips.map(&:days_between_purchase_and_trip).compact
+    @average_air_days_between = air_days.any? ? (air_days.sum.to_f / air_days.size).round(1) : 0
+
+    @total_air_mileage = air_trips.sum { |trip| trip.mileage.to_f }
+    @total_land_mileage = land_trips.sum { |trip| trip.mileage.to_f }
+
+    destination_frequency = @long_trips
+      .select { |trip| trip.destination_city.present? }
+      .group_by(&:destination_city)
+      .transform_values(&:count)
+
+    @most_frequent_destination = destination_frequency.max_by { |_city, count| count }
+
+    @total_air_cancellations = air_trips.count { |trip| trip.canceled == true }
+    @total_land_cancellations = land_trips.count { |trip| trip.canceled == true }
+
+    @air_cancellation_rate =
+      if @total_air_tickets.positive?
+        ((@total_air_cancellations.to_f / @total_air_tickets) * 100).round(1)
+      else
+        0
+      end
+
+    @sector_distribution = @long_trips
+      .select { |trip| trip.traveler_sector.present? }
+      .group_by(&:traveler_sector)
+      .transform_values(&:count)
+
+    @destination_distribution = destination_frequency
+  end
+
 end
